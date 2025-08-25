a09266efb3184d2b92a13c0d09f0d57a76db7834
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-08-25 23:25:19 +0600

    api

--- src/Components/Layouts/Product.jsx
+++ src/Components/Layouts/Product.jsx
@@ -4,12 +4,12 @@ import Tag from '/src/Components/Tag'
 import Headding from '../Headding'
 import { FaHeart,FaShoppingCart } from "react-icons/fa";
 import { TbRefresh } from "react-icons/tb";
-const Product = ({productImg,tag,ProductName,ProductPrice,text}) => {
+const Product = ({productImg,tag,ProductName,ProductPrice,text,className,imgclassName}) => {
   return (
    <>
-   <div className="">
+   <div className={`${className}`}>
         <div className="relative group">
-    <Image imgsrc={productImg}/>
+    <Image imgsrc={productImg} className={imgclassName}/>
     <Tag tagtext={tag} className={"absolute top-5 left-5"}/>
     <div className="pt-5 absolute top-[55%] opacity-0 group-hover:opacity-100 duration-700 left-0 right-0 bottom-0 bg-white ">
         <div className="flex gap-x-[15px] items-center justify-end pr-7.5">
@@ -26,9 +26,9 @@ const Product = ({productImg,tag,ProductName,ProductPrice,text}) => {
         </div>
     </div>
     </div>
-    <div className="flex items-center justify-between pt-6">
-        <Headding as={"h3"} text={ProductName} className={"text-xl font-bold text-[#262626]"}/>
-        <Headding as={"h3"} text={ProductPrice} className={" text-menuText"}/>
+    <div className="flex items-center justify-between pt-6 mb-10">
+        <Headding as={"h3"} text={ProductName} className={"text-xl font-bold text-[#262626] "}/>
+        <Headding as={"h3"} text={ProductPrice} className={" text-menuText "}/>
     </div>
     <Headding as={"h3"} text={text} className={" text-menuText"}/>
    </div>
--- src/Components/Layouts/SpecialOffers.jsx
+++ src/Components/Layouts/SpecialOffers.jsx
@@ -1,41 +1,55 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Product from './Product'
-import Table from "/src/assets/table.png"
-import HeadPhone from "/src/assets/Headphone.png"
-import Cap from "/src/assets/cap.png"
-import SunGlass from "/src/assets/sunglass.png"
-import Headding from '../Headding'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Container from "../Container";
+import Flex from "../Flex";
+import Headding from "../Headding";
+import Product from "./Product";
+
 const SpecialOffers = () => {
+let [alldata,setalldata]=useState([])
+useEffect(()=>{
+
+async function alldatas() {
+  let data=await axios.get("https://fakestoreapi.com/products/")
+   setalldata(data.data)
+  
+  
+}
+alldatas()
+},[])
+
   return (
     <>
       <div className="pb-[108px]">
         <Container>
-            
-             <Headding
+          <Headding
             as={"h3"}
             text={"Special Offers"}
             className={"font-bold text-black text-[39px] pb-[48px]"}
           />
-        <Flex className={"gap-x-10"}>
-            <Link to={"/"}>
-        <Product productImg={Cap} className={"w-full "} tag={"New"} ProductName={"Basic Crew Neck Tee"} ProductPrice={"$44.00"} text={"Black"}/></Link>
-
-        <Link to={"/"}>
-        <Product productImg={Table} className={"w-full "} tag={"New"} ProductName={"Basic Crew Neck Tee"} ProductPrice={"$44.00"} text={"Black"}/></Link>
-
-        <Link to={"/"}>
-        <Product productImg={HeadPhone} className={"w-full "} tag={"New"} ProductName={"Basic Crew Neck Tee"} ProductPrice={"$44.00"} text={"Black"}/></Link>
-
-        <Link to={"/"}>
-        <Product productImg={SunGlass} className={"w-full "} tag={"New"} ProductName={"Basic Crew Neck Tee"} ProductPrice={"$44.00"} text={"Black"}/></Link>
-        </Flex>
+          <Flex className={"  gap-x-[15px] gap-y-10 -mr-[80px] flex-wrap   "}>
+          {
+            alldata.slice(0,8).map((iteam)=>    
+          
+           
+              <Product
+                productImg={iteam.image}
+               className={'w-[23%]  px-4  h-[390px]  flex flex-col '}
+               imgclassName={' h-[270px] object-contain w-full m-auto pt-13 pb-6 bg-[#D8D8D8]/50'}
+                tag={"New"}
+                ProductName={iteam.title}
+                ProductPrice={`$${iteam.price}`}
+                
+              />
+         
+          
+         ) }
+          </Flex>
         </Container>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SpecialOffers
+export default SpecialOffers;
