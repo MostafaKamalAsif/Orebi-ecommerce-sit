1afdcd8dc7585332ece658f0220125ffaf682ce5
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-09-17 12:48:37 +0600

    pagination

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -4,83 +4,81 @@ import axios from 'axios'
 import Product from '../Layouts/Product'
 import Flex from '../Flex'
 
+
 const Shop = () => {
-  const page_size = 8
-  const [currentPage, setCurrentPage] = useState(1)
+const page_size=8
+  const [currantpage,setcurrantpage]=useState(0)
 
-  const [myproduct, setmyproduct] = useState([])
-  const [product, setproduct] = useState([])
-
-  useEffect(() => {
+//My API
+  const [myproduct,setmyproduct]=useState([])
+  useEffect(()=>{
     async function allmyproduct() {
-      try {
-        const mydata = await axios.get(
-          'https://mostafakamalasif.github.io/Orebi-API/index.json'
-        )
-        setmyproduct(mydata.data.data || [])
-      } catch (err) {
-        console.error(err)
-      }
+      let mydata=await axios.get("https://mostafakamalasif.github.io/Orebi-API/index.json")
+      setmyproduct(mydata.data.data)
     }
     allmyproduct()
-  }, [])
+  },[])
+  // Fake store product
+  const [product, setproduct]=useState([])
+  useEffect(()=>{
+ async function allproduct() {
+  let data=await axios.get("https://fakestoreapi.com/products/")
+  setproduct(data.data)
+ }
+ allproduct()
+  },[])
+  const allproducts=[...myproduct,...product]
+  const totallproduct=allproducts.length
+const onpageproduct=Math.ceil(totallproduct/page_size)
+const start=currantpage*page_size
+const end=start+page_size
+const curreantproduct=allproducts.slice(start,end)
 
-  useEffect(() => {
-    async function allproduct() {
-      try {
-        const data = await axios.get('https://fakestoreapi.com/products/')
-        setproduct(data.data || [])
-      } catch (err) {
-        console.error(err)
-      }
-    }
-    allproduct()
-  }, [])
+const pagechange=(page)=>{
+  setcurrantpage(page)
+}
+const gotoNextPage=()=>{
+  setcurrantpage((prev) =>prev+1)
+}
+const gotoPrevPage=()=>{
+  setcurrantpage((prev) =>prev-1)
+}
 
-  // Merge products
-  const allProducts = [...myproduct, ...product]
-  const totalProducts = allProducts.length
-  const totalPages = Math.ceil(totalProducts / page_size) || 1
-
-  // Slice products for current page
-  const startIndex = (currentPage - 1) * page_size
-  const currentProducts = allProducts.slice(startIndex, startIndex + page_size)
 
   return (
-    <div>
+     <>
+    <div className="">
       <Container>
-       
-
-        {/* Products */}
-        <Flex className="gap-x-[15px] gap-y-10 -mr-[80px] flex-wrap">
-          {currentProducts.map((item, i) => (
+           <div className="flex  justify-center ">
+            <span className='mr-2 border px-2' onClick={gotoPrevPage}>Prev</span>
+            {[...Array(onpageproduct).keys()].map((page,i)=>
+            <>
+           
+            <span className={`px-2 w-[25px] border mx-1 cursor-pointer  ${currantpage===page? 'bg-neutral-300  text-white ':""}` } onClick={()=>{pagechange(page)}} key={i}>{page+1}</span>
+             
+           </> )}
+           <span className= 'ml-2 border px-2' onClick={gotoNextPage}>Next </span>
+          </div>
+         <Flex className={"  gap-x-[15px] gap-y-10 -mr-[80px] flex-wrap   "}>
+   
+          {curreantproduct.map((item,x)=>
             <Product
-              key={item.id || i}
-              className="w-[23%] px-4 pt-2 h-[390px] flex flex-col"
-              productImg={item.img?.formats?.thumbnail?.url || item.image}
-              tag={item.img ? '10%' : 'New'}
-              ProductName={item.title}
-              ProductPrice={item.price}
-              imgclassName={!item.img ? 'h-[270px] object-contain w-full m-auto pt-13 pb-6 bg-[#D8D8D8]/50' : ''}
-            />
-          ))}
-        </Flex>
-         {/* Pagination */}
-        <div className="flex justify-center my-5">
-          {[...Array(totalPages)].map((_, index) => (
-            <span
-              key={index}
-              onClick={() => setCurrentPage(index + 1)}
-              className={`px-3 py-1 mx-1 border rounded cursor-pointer ${
-                currentPage === index + 1 ? 'bg-blue-500 text-white' : ''
-              }`}
-            >
-              {index + 1}
-            </span>
-          ))}
-        </div>
+            key={x}
+                productImg={item.img?.formats?.thumbnail?.url || item.image}
+               className={'w-[23%]  px-4  h-[390px]  flex flex-col '}
+               imgclassName={!item.img? ' h-[270px] object-contain w-full m-auto pt-13 pb-6 bg-[#D8D8D8]/50':""}
+                tag={item.image?"New":"10%"}
+                ProductName={ item.title }
+               ProductPrice={item.price}
+                
+              />
+          )}
+          </Flex>
+
+         
       </Container>
     </div>
+    </>
   )
 }
 
