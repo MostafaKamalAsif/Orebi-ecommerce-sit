d5623eefdcf6ec334d4d87e549ebdda35907fee6
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-09-22 21:46:42 +0600

    ...................

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Headding from './Headding'
 
 
-const Input = ({type,placeholder,divclassName,className,title,icon,value,onChange,onBlur}) => {
+const Input = ({type,placeholder,divclassName,className,title,icon,value}) => {
   return (
     <>
    
      <div className={divclassName}>
       <Headding as={"h3"} text={title} className={className}/>
        <div className="flex items-center">
-<input type={type} placeholder={placeholder} value={value} onChange={onChange} onFocus={onFocus} onBlur={onBlur} className={`w-[500px] border-b-1 outline-0 border-[#F0F0F0] text-[14px] py-4 `}/>
+<input type={type} placeholder={placeholder} value={value}  className={`w-[500px] border-b-1 outline-0 border-[#F0F0F0] text-[14px] py-4 `}/>
      <Headding as={"h3"} text={icon}/>
      </div>
      </div>  
