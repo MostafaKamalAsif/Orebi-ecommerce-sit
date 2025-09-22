c8a5b0c89b8b94bf135107e9957436dfd8f89760
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-09-22 12:55:50 +0600

    ....

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -16,36 +16,17 @@ import { FaUserPlus } from "react-icons/fa";
 import { decrement, increment } from "../../Slices/AddToCart";
 import SmDevice from "./SmDevice";
 import axios from "axios";
+import Autosearch from "./Autosearch";
 
 const Header = () => {
-  let[input, setinput]=useState("")
-  let[searchitem, setsearchitem]=useState([])
-  let[barshow, setbarshow]=useState(false)
+ 
 
   let [show, setshow] = useState(false);
   let [showUser, setShowUser] = useState(false);
   let [showcart, setShowcart] = useState(false);
   let data = useSelector((state) => state.addcart.value);
   const dispatchcounter = useDispatch();
-  //search item api
-  useEffect(()=>{
-    async function allproduct() {
-      let myapi=await axios.get("https://mostafakamalasif.github.io/Orebi-API/index.json?q=" +input)
-      let fakeapi=await axios.get("https://fakestoreapi.com/products/?q=" +input)
-      let all=[...myapi.data.data,...fakeapi.data]
-
-   
-   
-    // Filter locally based on input
-    let filtered = all.filter((item) =>
-      item.title.toLowerCase().includes(input.toLowerCase())
-    );
-
-    setsearchitem(filtered);
-   }
-
-    allproduct()
-  },[input])
+  
   // close Catogory in Outside click start
   const catgoryRef = useRef();
   useEffect(() => {
@@ -214,18 +195,7 @@ const Header = () => {
               />
               
               <IoSearch className="absolute right-[3%] top-[50%] -translate-y-1/2 cursor-pointer" />
-              { barshow &&(
-               <div className="absolute top-[55px]  w-601px max-h-[200px] bg-white overflow-x-scroll ">
-              {searchitem.map((item)=>
-              <>
-              <ul className="flex items-center gap-3 py-1 hover:bg-neutral-200 duration-150 cursor-pointer">
-              <li className="w-[7%] "><img src={item.img?.formats?.thumbnail?.url || item.image}  /></li>
-              <li className="text-black " >{item.title}</li>
-              </ul>
-              </>
-              )}
-              </div>
-             ) }
+             <Autosearch/>
             </div>
            
             <div className="">
