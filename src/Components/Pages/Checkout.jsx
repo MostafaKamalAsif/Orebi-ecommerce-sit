acd76b1c8c4601d3f6e5d96a7fdd7f8032ad6333
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-10-19 16:02:47 +0600

    Half checkout complete

--- src/Components/Pages/Checkout.jsx
+++ src/Components/Pages/Checkout.jsx
@@ -3,6 +3,7 @@ import Container from '../Container'
 import Headding from '../Headding'
 import Flex from '../Flex'
 import { Link } from 'react-router-dom'
+import Input from '../Input'
 
 const Checkout = () => {
   return (
@@ -20,8 +21,30 @@ const Checkout = () => {
                           className={"cursor-pointer"}
                         />
                       </Flex>
-            <Headding as={"h3"} text={"Have a coupon? Click here to enter your code"} className={"pt-[100px] text-[#767676] text-[16px]"}/>
-                      
+            <Headding as={"h3"} text={"Have a coupon? Click here to enter your code"} className={"py-[100px] text-[#767676] text-[16px]"}/>
+            <Headding as={"h3"} text={"Billing Details"} className={"pb-[42px] font-bold text-[#262626] text-[39px]"}/>
+                      <Flex className={"gap-x-7.5"}>
+                        <Input title={"First Name *"} className={"font-semibold"} type={"text"} placeholder={"First Name"}/>
+                        <Input title={"Last Name *"}  className={"font-semibold"}  type={"text"} placeholder={"Last Name"}/>
+                      </Flex>
+                        <Input title={"Companye Name (optional)"} className={"font-semibold pt-6"}  type={"text"}placeholder={"Company Name"}/>
+                        <Input title={"Country *"} className={"font-semibold pt-6"} placeholder={"Please select"} />
+                        <Input title={"Street Address *"} className={"font-semibold  pt-6"}  type={"text"}placeholder={"House number and street name"}/>
+                        <Input    placeholder={"Apartment, suite, unit etc. (optional)"}/>
+                        <Input title={"Town/City *"}  className={"font-semibold pt-6"}  type={"text"} placeholder={"Town/City"}/>
+                        <Input title={"County (optional)"}  className={"font-semibold pt-6"} type={"text"}  placeholder={"County"}/>
+                        <Input title={"Post Code *"}  className={"font-semibold pt-6"}  type={"text"} placeholder={"Post Code"}/>
+                        <Input title={"Phone *"}  className={"font-semibold pt-6"}  type={"text"} placeholder={"Phone"}/>
+                        <Input title={"Email Address *"}  className={"font-semibold pt-6"} type={"email"} placeholder={"Email"}/>
+ <div className="py-[100px]">
+            <Headding as={"h3"} text={"Additional Information"} className={"pb-[42px] font-bold text-[#262626] text-[39px]"}/>
+           
+                        <Input title={"Other Notes (optional)"}  className={"font-semibold pt-6"}  type={"text"} placeholder={"Notes about your order, e.g. special notes for delivery."}/>
+            
+          
+
+ </div>
+
         </Container>
     </div>
     </>
