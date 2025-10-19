51170efd0202c48b5245710f32abfbb04e92838b
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-10-19 12:28:53 +0600

    Checkout page start

--- src/Components/Pages/Checkout.jsx
+++ src/Components/Pages/Checkout.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Container from '../Container'
+import Headding from '../Headding'
+import Flex from '../Flex'
+import { Link } from 'react-router-dom'
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <>
+    <div className="py-[128px]">
+        <Container>
+            <Headding as={"h1"} text={"Checkout"} className={"font-bold text-[#262626] text-3xl"}/>
+                   <Flex className={"flex items-center gap-x-1.5 pt-3 text-menuText "}>
+                        <Link to={"/"}>
+                          <Headding as={"h4"} text={"Home"} />
+                        </Link>
+                        <Headding
+                          as={"h4"}
+                          text={"> Checkout"}
+                          className={"cursor-pointer"}
+                        />
+                      </Flex>
+            <Headding as={"h3"} text={"Have a coupon? Click here to enter your code"} className={"pt-[100px] text-[#767676] text-[16px]"}/>
+                      
+        </Container>
+    </div>
+    </>
   )
 }
 
